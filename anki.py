7f1706ee19393ad9eaaa71cf5ed25159f2a23622
armel.vidali <vidali.armel@gmail.com>
2023-12-09 16:42:17 +0100

    corrected card creation - audio format

--- anki.py
+++ anki.py
@@ -5,12 +5,19 @@ import json
 ANKI_URL = "http://localhost:8765"
 
 
-def add_card(deck_name, front, word_translation, back_sentences, audio_files):
+def add_card(deck_name, front, word_translation, back_sentences):
     # merge audio and sentences
-    single_word_translation = f"<p>{word_translation}</p>"
+    single_word_translation = f"<p><span style='font-size: 35px;'>{word_translation}</span></p>"
 
-    back_content = "<br>".join([f"<p>{back_sentences[i][0]} {back_sentences[i][1]} [sound:{audio_files[i]}]</p>"
-                                for i in range(len(back_sentences))])
+    back_content = ""
+    # handle poentially missing audio url
+    for i in range(len(back_sentences)):
+        if len(back_sentences[i]) == 3:
+            back_content += f"<p>{back_sentences[i][0]}<br><span style='font-size: 45px;'>{back_sentences[i][1]}</span><br><audio controls><source src={back_sentences[i][2]} type=\"audio/mp3\"></audio></p>"
+
+        else:
+            back_content += f"<p>{back_sentences[i][0]}<br>{back_sentences[i][1]} ]</p>"
+
     back_content = single_word_translation + back_content
 
     # Add a card to a specific deck
@@ -36,24 +43,20 @@ def add_card(deck_name, front, word_translation, back_sentences, audio_files):
     return response.json()
 
 
-with open("translations_results.json", 'r', encoding='utf-8') as json_file:
+with open("translations_results_11k.json", 'r', encoding='utf-8') as json_file:
     word_list = json.load(json_file)
-
+nb_mots = 0
 for keyword in word_list:
+    print(f"{nb_mots}/11000")
     word_translation = ""
     sentences = []
-    audios = []
     # Build sentence list
     word_translation = word_list[keyword]["result"]
-    audios.append(
-        f"C:/Users/vidal/AppData/Roaming/Anki2/User 1/collection.media/{keyword}_translation_0.mp3")
 
     for i in range(int((len(word_list[keyword])))-1):
         sentences.append(word_list[keyword][f"sample_{i}"])
-        # build the list of audios files
-        audios.append(
-            f"C:/Users/vidal/AppData/Roaming/Anki2/User 1/collection.media/{keyword}_sample_sentence_translation_{i}.mp3")
 
     # add the card with audios to the deck
     card_addition_result = add_card(
-        "Tunisian_arabic_derja_top5000words", keyword, word_translation, sentences, audios)
+        "Tunisian_arabic_derja_top5000words", keyword, word_translation, sentences)
+    nb_mots += 1
